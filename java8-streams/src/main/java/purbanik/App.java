0d3016c5490de30db48cb58e9911b4bc5ec0b047
subieslaw <subieslaw@gmail.com>
2016-11-09 22:38:53 +0100

    initial project setup

---
+++ java8-streams/src/main/java/purbanik/App.java
@@ -0,0 +1,13 @@
+package purbanik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
